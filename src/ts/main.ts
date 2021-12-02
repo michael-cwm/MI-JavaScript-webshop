8670edbe06876a8a5e485894a4e796f6e4310a1a
michael-cwm <michaelcwm@hotmail.com>
2021-12-02 17:25:17 +0100

    added burger nav

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,9 +1,15 @@
-/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
-function myFunction() {
-    var x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
+window.onload = function(){
+
+const menuBtn = document.querySelector('.menu-btn');
+let menuOpen = false;
+menuBtn.addEventListener('click', () => {
+  if(!menuOpen) {
+    menuBtn.classList.add('open');
+    menuOpen = true;
+  } else {
+    menuBtn.classList.remove('open');
+    menuOpen = false;
   }
+});
+
+}
