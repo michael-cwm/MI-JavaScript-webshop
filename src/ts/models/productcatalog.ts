dd7a0e29626b033722ab6217b25f4bb850d29e98
Malin Nilsson <malinnilsson87@gmail.com>
2021-12-03 20:47:09 +0100

    Added products

--- src/ts/models/productcatalog.ts
+++ src/ts/models/productcatalog.ts
@@ -3,56 +3,64 @@ export const productcatalog = [
         name: "Sov i skogen",
         price: 5999,
         users: "1-2",
-        img: "../assets/product-1.jpg"
+        img: "https://images.unsplash.com/photo-1551927411-95e412943b58?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=749&q=80",
+        details: "Lorem ipsum lorem ipsum lorem ipsum"
     },
 
     {
         name: "Öl med en vy",
         price: 1999,
         users: "1-2",
-        img: "../../assets/product-1.jpg"
+        img: "https://images.unsplash.com/photo-1517480625158-292a09aee755?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+        details: "Lorem ipsum lorem ipsum lorem ipsum"
     },
 
     {
         name: "Rymdresa",
         price: 1999000,
         users: "1",
-        img: "../../assets/product-1.jpg"
+        img: "https://images.unsplash.com/photo-1517480625158-292a09aee755?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+        details: "Lorem ipsum lorem ipsum lorem ipsum"
     },
 
     {
         name: "Bo på Richard Bransons ö",
         price: 590000,
         users: "1-20",
-        img: "../../assets/product-1.jpg"
+        img: "https://images.unsplash.com/photo-1551927411-95e412943b58?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=749&q=80",
+        details: "Lorem ipsum lorem ipsum lorem ipsum"
     },
 
     {
         name: "Naturvandring i Jason Voorhees fotspår",
         price: 2599,
-        users: ["1-5"],
-        img: "../../assets/product-1.jpg"
+        users: "1-5",
+        img: "https://images.unsplash.com/photo-1517480625158-292a09aee755?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+        details: "Lorem ipsum lorem ipsum lorem ipsum"
     },
 
     {
         name: "Wingsuit (för dig med dödslängtan)",
         price: 7599,
-        users: ["1-2"],
-        img: "../../assets/product-1.jpg"
+        users: "1-2",
+        img: "https://images.unsplash.com/photo-1517480625158-292a09aee755?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+        details: "Lorem ipsum lorem ipsum lorem ipsum"
     },
 
     {
         name: "Simma med fiskarna",
         price: 19999,
-        users: ["1-2"],
-        img: "../../assets/product-1.jpg"
+        users: "1-2",
+        img: "https://images.unsplash.com/photo-1517480625158-292a09aee755?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+        details: "Lorem ipsum lorem ipsum lorem ipsum"
     },
 
     {
         name: "Golden-mys",
         price: 599,
-        users: ["1"],
-        img: "../../assets/product-1.jpg"
+        users: "1",
+        img: "https://images.unsplash.com/photo-1517480625158-292a09aee755?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+        details: "Lorem ipsum lorem ipsum lorem ipsum"
     }
 ];
 
