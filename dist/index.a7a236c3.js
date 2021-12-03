ab30817b1b1b11ff9ce4d4c384ecb3cf00f77959
michael-cwm <michaelcwm@hotmail.com>
2021-12-03 12:57:23 +0100

    updated header and navbar

--- dist/index.a7a236c3.js
+++ dist/index.a7a236c3.js
@@ -11,5 +11,10 @@ window.onload = function() {
         }
     });
 };
+function myFunction() {
+    var x = document.getElementById("Demo");
+    if (x.className.indexOf("w3-show") == -1) x.className += " w3-show";
+    else x.className = x.className.replace(" w3-show", "");
+}
 
 //# sourceMappingURL=index.a7a236c3.js.map
