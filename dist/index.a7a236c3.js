630c67c1114d43b97c78cfea1e8b3a813d807344
Malin Nilsson <malinnilsson87@gmail.com>
2021-12-04 15:10:08 +0100

    Created function to load html on landing page

--- dist/index.a7a236c3.js
+++
@@ -1,2 +0,0 @@
-
-//# sourceMappingURL=index.a7a236c3.js.map
---
+++ src/ts/main.ts
@@ -0,0 +1,88 @@
+import { productcatalog } from "./models/productcatalog";
+
+window.onload = function() {
+    loadHTML() 
+}
+
+// Load HTML on landing page
+function loadHTML() {
+let container: HTMLDivElement = document.querySelector(".products");
+container.classList.add("products");
+
+// Loop through the product catalog
+for (let i = 0; i < productcatalog.length; i++) {
+    console.log(productcatalog);
+
+    // Product card
+    let product: HTMLDivElement = document.createElement("div");
+    product.classList.add("product");
+    container.appendChild(product);
+
+    // Image container
+    let imgContainer: HTMLDivElement = document.createElement("div");
+    imgContainer.classList.add("img-container");
+    product.append(imgContainer);
+
+    // Product image
+    let image: HTMLImageElement = document.createElement("img");
+    image.setAttribute("src", productcatalog[i].img); 
+    imgContainer.appendChild(image);
+
+    // Container for product info
+    let productCard: HTMLDivElement = document.createElement("div");
+    productCard.classList.add("product-card");
+    imgContainer.after(productCard);
+
+    // Product name
+    let productName: HTMLSpanElement = document.createElement("span");
+    productName.classList.add("product-name");
+    productName.innerHTML = productcatalog[i].name;
+
+
+    // Stars
+    let productRating: HTMLSpanElement = document.createElement("span");
+    productRating.classList.add("star-rating");
+    productRating.innerHTML = "";
+
+    // Users
+    let productUsers: HTMLSpanElement = document.createElement("span");
+    productUsers.classList.add("users");
+    productUsers.innerHTML = productcatalog[i].users;
+
+    productCard.appendChild(productName);
+    productName.after(productRating);
+    productRating.after(productUsers);
+    
+    // Product details
+    let productDetails: HTMLParagraphElement = document.createElement("p");
+    productDetails.innerHTML = productcatalog[i].details;
+    productDetails.classList.add("product-details");
+    productUsers.after(productDetails);
+
+    // Product footer
+    let productFooter: HTMLDivElement = document.createElement("div");
+    productFooter.classList.add("product-footer");
+    productDetails.after(productFooter);
+
+    // Price
+    let priceContainer: HTMLDivElement = document.createElement("div");
+    priceContainer.classList.add("price-container");
+    productFooter.appendChild(priceContainer);
+    let price: HTMLSpanElement = document.createElement("span");
+    price.classList.add("price");
+    price.innerHTML = productcatalog[i].price.toLocaleString() + " kr";
+    priceContainer.appendChild(price);
+
+    // Buttons
+    let productButtons: HTMLDivElement = document.createElement("div");
+    productButtons.classList.add("product-buttons");
+    priceContainer.after(productButtons);
+    let buttonBuy: HTMLButtonElement = document.createElement("button");
+    buttonBuy.innerHTML = "Mer info";
+    let buttonInfo: HTMLButtonElement = document.createElement("button");
+    buttonInfo.innerHTML = "Köp";
+    productButtons.appendChild(buttonBuy);
+    productButtons.appendChild(buttonInfo);
+
+}
+}
