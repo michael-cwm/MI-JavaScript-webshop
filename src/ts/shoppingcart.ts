1a5e2fd17f9325f1f2ab7395e4aa4aad7c4d75ca
Malin Nilsson <malinnilsson87@gmail.com>
2021-12-08 22:00:24 +0100

    Added layout and styles to shopping cart

--- src/ts/shoppingcart.ts
+++ src/ts/shoppingcart.ts
@@ -2,91 +2,97 @@ import {
     productcatalog
 } from "./models/productcatalog";
 
-window.onload = function() {
+import {
+    shoppingCartArray
+} from "./main";
+
+
+
+window.onload = function () {
     shoppingCart()
 
+    console.log(shoppingCartArray)
 }
 
 
 
 function shoppingCart() {
-
     let wrapper: HTMLDivElement = document.querySelector(".wrapper");
     let arrowContainer = document.createElement("div");
     arrowContainer.classList.add("back-arrow-container");
     let arrow = document.createElement("i");
     arrowContainer.appendChild(arrow);
     arrow.setAttribute("class", "bi bi-arrow-left");
-
     wrapper.before(arrowContainer);
-
     arrow.addEventListener("click", () => {
         window.history.back();
 
     });
 
-    let shoppingcartContainer: HTMLDivElement = document.createElement("div");
-    shoppingcartContainer.id = "shoppingcart-container";
 
-    wrapper.appendChild(shoppingcartContainer);
+    for (let i = 0; i < productcatalog.length; i++) {
+        let shoppingcartContainer: HTMLDivElement = document.createElement("div");
+        shoppingcartContainer.id = "shoppingcart-container";
+        wrapper.appendChild(shoppingcartContainer);
 
-    // Header
-    let headerContainer: HTMLDivElement = document.createElement("div");
-    headerContainer.classList.add("cart-header-container");
-    shoppingcartContainer.appendChild(headerContainer);
 
-    let header: HTMLHeadingElement = document.createElement("h1");
-    header.innerHTML = "Din varukorg";
-    headerContainer.appendChild(header);
 
-    // Cart Div
-    let items: HTMLDivElement = document.createElement("div");
-    items.classList.add("cart-items");
-    shoppingcartContainer.appendChild(items);
-    
+        // Cart Div
+        let items: HTMLDivElement = document.createElement("div");
+        items.classList.add("cart-items");
+        shoppingcartContainer.appendChild(items);
 
-    // Cart Image Section
-    let cartImageSection: HTMLDivElement = document.createElement("div");
-    cartImageSection.classList.add("cart-images");
 
-    let cartImage: HTMLImageElement = document.createElement("img");
-    cartImage.setAttribute("src", "");
-    cartImageSection.appendChild(cartImage);
+        // Cart Image Section
+        let cartImageSection: HTMLDivElement = document.createElement("div");
+        cartImageSection.classList.add("cart-images");
 
-    items.appendChild(cartImageSection);
+        let cartImage: HTMLImageElement = document.createElement("img");
+        cartImage.setAttribute("src", productcatalog[i].img);
+        cartImageSection.appendChild(cartImage);
 
-    // Cart Info Section
-    let item: HTMLDivElement = document.createElement("div");
-    item.classList.add("cart-item");
-    items.appendChild(item);
+        items.appendChild(cartImageSection);
 
-    // Product name
-    let name: HTMLSpanElement = document.createElement("span");
-    name.classList.add("product-name");
-    name.innerHTML = "Produktnamn";
-    item.appendChild(name);
+        // Cart Info Section
+        let item: HTMLDivElement = document.createElement("div");
+        item.classList.add("cart-item");
+        items.appendChild(item);
 
-    // Price
-    let price: HTMLSpanElement = document.createElement("span");
-    price.classList.add("price");
-    price.innerHTML = "Pris";
-    name.after(price);
+        // Product name
+        let name: HTMLSpanElement = document.createElement("span");
+        name.classList.add("product-name");
+        name.innerHTML = productcatalog[i].name;
+        item.appendChild(name);
 
-    // Cart Icons
-    let cartIcons: HTMLDivElement = document.createElement("div");
-    cartIcons.classList.add("cart-icons");
-    item.after(cartIcons);
+        // Price
+        let price: HTMLSpanElement = document.createElement("span");
+        price.classList.add("price");
+        price.innerHTML = productcatalog[i].price.toLocaleString() + " kr";
+        name.after(price);
 
-    let plusicon = document.createElement("i");
-    plusicon.setAttribute("class", "bi bi-plus-lg");
-    cartIcons.appendChild(plusicon);
+        // Details
+        let details: HTMLSpanElement = document.createElement("p");
+        details.innerHTML = productcatalog[i].details;
+        details.classList.add("details");
+        price.after(details);
 
-    let productAmount: HTMLSpanElement = document.createElement("span");
-    cartIcons.appendChild(productAmount);
-    productAmount.innerHTML = "Antal";
+        // Cart Icons
+        let cartIcons: HTMLDivElement = document.createElement("div");
+        cartIcons.classList.add("cart-icons");
+        item.after(cartIcons);
 
-    let minusicon = document.createElement("i");
-    minusicon.setAttribute("class", "bi bi-dash-lg");
-    cartIcons.appendChild(minusicon);
+        let plusicon = document.createElement("i");
+        plusicon.setAttribute("class", "bi bi-plus-lg");
+        cartIcons.appendChild(plusicon);
 
+        let productAmount: HTMLSpanElement = document.createElement("span");
+        cartIcons.appendChild(productAmount);
+        productAmount.classList.add("product-amount");
+        productAmount.innerHTML = "Antal";
+
+        let minusicon = document.createElement("i");
+        minusicon.setAttribute("class", "bi bi-dash-lg");
+        cartIcons.appendChild(minusicon);
+    }
 }
+
