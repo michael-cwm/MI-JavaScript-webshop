31cd3af4374fc7de43d6f3672c43e10bba475366
EmmaNGagen <emma.n.gagen@outlook.com>
2021-12-10 16:14:07 +0100

    animation modal

--- src/ts/landingpage.ts
+++ src/ts/landingpage.ts
@@ -1,152 +1,142 @@
-import {
-    shoppingCartArray
-} from "./main";
+import { shoppingCartArray } from "./main";
 
-import {
-    productcatalog
-} from "./models/productcatalog";
-
+import { productcatalog } from "./models/productcatalog";
 
 export function loadLandingPage() {
-    // Container for landing page
-    let wrapper: HTMLDivElement = document.querySelector(".wrapper");
-    let container: HTMLDivElement = document.createElement("div");
-    container.classList.add("products");
-    wrapper.appendChild(container);
+  // Container for landing page
+  let wrapper: HTMLDivElement = document.querySelector(".wrapper");
+  let container: HTMLDivElement = document.createElement("div");
+  container.classList.add("products");
+  wrapper.appendChild(container);
 
-    // Modal
-    let modal: HTMLDivElement = document.createElement("div");
+  // Modal
+  let modal: HTMLDivElement = document.createElement("div");
 
-    window.onclick = function (event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
     }
-    modal.classList.add("modalContainer");
-    modal.id = "modal-container"
+  };
+  modal.classList.add("modalContainer");
+  modal.id = "modal-container";
 
-    let modalContent: HTMLDivElement = document.createElement("div");
-    modalContent.classList.add("modalContent");
-    modalContent.id = "modal-content"
+  let modalContent: HTMLDivElement = document.createElement("div");
+  modalContent.classList.add("modalContent");
+  modalContent.id = "modal-content";
 
-    container.after(modal)
-    modal.appendChild(modalContent);
+  container.after(modal);
+  modal.appendChild(modalContent);
 
+  let close: HTMLSpanElement = document.createElement("span");
+  close.classList.add("bi", "bi-x");
+  modalContent.appendChild(close);
 
-    let close: HTMLSpanElement = document.createElement("span");
-    close.classList.add("bi", "bi-x");
-    modalContent.appendChild(close);
+  let modalHeader: HTMLParagraphElement = document.createElement("p");
+  modalHeader.innerHTML = "Upplevelsen lades till i kundkorgen!";
+  modalHeader.classList.add("modalHeader");
+  modalContent.appendChild(modalHeader);
+  close.addEventListener("click", () => {
+    modal.style.display = "none";
+  });
 
+  let cartButton: HTMLAnchorElement = document.createElement("a");
+  cartButton.innerHTML = "Gå till kassan";
+  cartButton.classList.add("button-buy");
+  cartButton.setAttribute("href", "checkout.html");
+  modalHeader.after(cartButton);
 
-    let modalHeader: HTMLParagraphElement = document.createElement("p");
-    modalHeader.innerHTML = "Upplevelsen lades till i kundkorgen!";
-    modalHeader.classList.add("modalHeader");
-    modalContent.appendChild(modalHeader);
-    close.addEventListener("click", () => {
-        modal.style.display = "none";
-    })
+  let gifContainer: HTMLDivElement = document.createElement("div");
+  let img: HTMLImageElement = document.createElement("img");
+  gifContainer.classList.add("gif");
+  gifContainer.appendChild(img);
+  cartButton.after(gifContainer);
 
-    let cartButton: HTMLAnchorElement = document.createElement("a");
-    cartButton.innerHTML = "Gå till kassan";
-    cartButton.classList.add("button-buy");
-    cartButton.setAttribute("href", "checkout.html");
-    modalHeader.after(cartButton);
+  // Loop through the product catalog
+  for (let i = 0; i < productcatalog.length; i++) {
+    // Product card
+    let product: HTMLDivElement = document.createElement("div");
+    product.classList.add("product");
+    container.appendChild(product);
 
-    let gifContainer: HTMLDivElement = document.createElement("div");
-    let img: HTMLImageElement = document.createElement("img");
-    gifContainer.classList.add("gif");
-    gifContainer.appendChild(img)
-    cartButton.after(gifContainer);
+    // Image container
+    let imgContainer: HTMLDivElement = document.createElement("div");
+    imgContainer.classList.add("img-container");
+    product.append(imgContainer);
 
+    // Product image
+    let image: HTMLImageElement = document.createElement("img");
+    image.setAttribute("src", productcatalog[i].img);
+    imgContainer.appendChild(image);
 
-    // Loop through the product catalog
-    for (let i = 0; i < productcatalog.length; i++) {
+    // Container for product info
+    let productCard: HTMLDivElement = document.createElement("div");
+    productCard.classList.add("product-card");
+    imgContainer.after(productCard);
 
-        // Product card
-        let product: HTMLDivElement = document.createElement("div");
-        product.classList.add("product");
-        container.appendChild(product);
+    // Product name
+    let productName: HTMLSpanElement = document.createElement("span");
+    productName.classList.add("product-name");
+    productName.innerHTML = productcatalog[i].name;
 
-        // Image container
-        let imgContainer: HTMLDivElement = document.createElement("div");
-        imgContainer.classList.add("img-container");
-        product.append(imgContainer);
+    // Stars
+    let productRating: HTMLSpanElement = document.createElement("span");
+    productRating.classList.add("star-rating");
+    productRating.innerHTML = "";
 
-        // Product image
-        let image: HTMLImageElement = document.createElement("img");
-        image.setAttribute("src", productcatalog[i].img);
-        imgContainer.appendChild(image);
+    // Users
+    let productUsers: HTMLSpanElement = document.createElement("span");
+    productUsers.classList.add("users");
+    productUsers.innerHTML = productcatalog[i].users;
 
-        // Container for product info
-        let productCard: HTMLDivElement = document.createElement("div");
-        productCard.classList.add("product-card");
-        imgContainer.after(productCard);
+    productCard.appendChild(productName);
+    productName.after(productRating);
+    productRating.after(productUsers);
 
-        // Product name
-        let productName: HTMLSpanElement = document.createElement("span");
-        productName.classList.add("product-name");
-        productName.innerHTML = productcatalog[i].name;
+    // Product details
+    let productDetails: HTMLParagraphElement = document.createElement("p");
+    productDetails.innerHTML = productcatalog[i].details;
+    productDetails.classList.add("product-details");
+    productUsers.after(productDetails);
 
-        // Stars
-        let productRating: HTMLSpanElement = document.createElement("span");
-        productRating.classList.add("star-rating");
-        productRating.innerHTML = "";
+    // Product footer
+    let productFooter: HTMLDivElement = document.createElement("div");
+    productFooter.classList.add("product-footer");
+    productDetails.after(productFooter);
 
-        // Users
-        let productUsers: HTMLSpanElement = document.createElement("span");
-        productUsers.classList.add("users");
-        productUsers.innerHTML = productcatalog[i].users;
+    // Price
+    let priceContainer: HTMLDivElement = document.createElement("div");
+    priceContainer.classList.add("price-container");
+    productFooter.appendChild(priceContainer);
+    let price: HTMLSpanElement = document.createElement("span");
+    price.classList.add("price");
+    price.innerHTML = productcatalog[i].price.toLocaleString() + " kr";
+    priceContainer.appendChild(price);
 
-        productCard.appendChild(productName);
-        productName.after(productRating);
-        productRating.after(productUsers);
+    // Buttons
+    let productButtons: HTMLDivElement = document.createElement("div");
+    productButtons.classList.add("product-buttons");
+    priceContainer.after(productButtons);
+    let buttonBuy: HTMLAnchorElement = document.createElement("a");
+    buttonBuy.classList.add("button-buy");
+    buttonBuy.innerHTML = "Köp";
+    buttonBuy.id = "btn-menu";
+    buttonBuy.setAttribute("data-id", productcatalog[i].id.toString());
 
-        // Product details
-        let productDetails: HTMLParagraphElement = document.createElement("p");
-        productDetails.innerHTML = productcatalog[i].details;
-        productDetails.classList.add("product-details");
-        productUsers.after(productDetails);
+    let buttonInfo: HTMLAnchorElement = document.createElement("a");
+    buttonInfo.classList.add("button-info");
+    buttonInfo.setAttribute("data-id", productcatalog[i].id.toString());
+    buttonInfo.innerHTML = "Mer info";
 
-        // Product footer
-        let productFooter: HTMLDivElement = document.createElement("div");
-        productFooter.classList.add("product-footer");
-        productDetails.after(productFooter);
+    productButtons.appendChild(buttonInfo);
+    productButtons.appendChild(buttonBuy);
 
-        // Price
-        let priceContainer: HTMLDivElement = document.createElement("div");
-        priceContainer.classList.add("price-container");
-        productFooter.appendChild(priceContainer);
-        let price: HTMLSpanElement = document.createElement("span");
-        price.classList.add("price");
-        price.innerHTML = productcatalog[i].price.toLocaleString() + " kr";
-        priceContainer.appendChild(price);
+    buttonInfo.addEventListener("click", () => {
+      location.href = "product.html?id=" + productcatalog[i].id;
+    });
 
-        // Buttons
-        let productButtons: HTMLDivElement = document.createElement("div");
-        productButtons.classList.add("product-buttons");
-        priceContainer.after(productButtons);
-        let buttonBuy: HTMLAnchorElement = document.createElement("a");
-        buttonBuy.classList.add("button-buy");
-        buttonBuy.innerHTML = "Köp";
-        buttonBuy.id = "btn-menu";
-        buttonBuy.setAttribute("data-id", productcatalog[i].id.toString());
-
-        let buttonInfo: HTMLAnchorElement = document.createElement("a");
-        buttonInfo.classList.add("button-info");
-        buttonInfo.setAttribute("data-id", productcatalog[i].id.toString());
-        buttonInfo.innerHTML = "Mer info";
-
-        productButtons.appendChild(buttonInfo);
-        productButtons.appendChild(buttonBuy);
-
-        buttonInfo.addEventListener("click", () => {
-            location.href = "product.html?id=" + productcatalog[i].id;
-        })
-
-        buttonBuy.addEventListener("click", () => {
-            shoppingCartArray.push(productcatalog[i].id);
-            modal.style.display = "block";
-        })
-
-    }
+    buttonBuy.addEventListener("click", () => {
+      shoppingCartArray.push(productcatalog[i].id);
+      modal.style.display = "block";
+    });
+  }
 }
