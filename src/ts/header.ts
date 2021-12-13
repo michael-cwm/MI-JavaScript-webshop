7a68b9e21e987eb70d9c1100242e26ad95335c48
michael-cwm <michaelcwm@hotmail.com>
2021-12-13 12:25:02 +0100

    added functionality to header

--- src/ts/header.ts
+++ src/ts/header.ts
@@ -1,31 +1,7 @@
-window.onload = function(){
-
-    // const menuBtn = document.querySelector('.menu-btn');
-    // let menuOpen = false;
-    // menuBtn.addEventListener('click', () => {
-    //   if(!menuOpen) {
-    //     menuBtn.classList.add('open');
-    //     menuOpen = true;
-    //   } else {
-    //     menuBtn.classList.remove('open');
-    //     menuOpen = false;
-    //   }
-    // });
+window.onload = function(){}
     
     
     
-    
-    }
-    
-    // let test = document.getElementById('halloj');
-    // console.log(test);
-    
-    // test.addEventListener("click", burgerToggle);
-    
-    // function burgerToggle(){
-    //   let element:HTMLDivElement = document.getElementById("burgerDropdown") as HTMLDivElement;
-    //   element.classList.toggle("burgerStyle");
-    // }
     
 
 
@@ -53,4 +29,18 @@ window.onload = function(){
       element.classList.toggle("burgerStyle");
     }
 
+    document.getElementById("homeButton").addEventListener("click", () => {
+      window.location.href = "index.html";
+    });
+
+    document.getElementById("aboutButton").addEventListener("click", () => {
+      window.location.href = "#about";
+    });
+
+    document.getElementById("contactButton").addEventListener("click", () => {
+      window.location.href = "#contact";
+    });
+
 }
+
+
