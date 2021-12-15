ec15a84588691dff25647cd2fccb2f346ec16127
Malin Nilsson <malinnilsson87@gmail.com>
2021-12-15 16:24:16 +0100

    Added function to sum up total on checkout page

--- src/ts/checkout.ts
+++ src/ts/checkout.ts
@@ -1,126 +1,134 @@
+import {
+  displayItemAmount
+} from "./shoppingcart";
 
-    import { displayItemAmount } from "./shoppingcart";
-
-    displayItemAmount()
-
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    var forms = document.querySelectorAll(".needs-validation");
-
-    console.log(forms);
-  
-    // Loop over them and prevent submission
-    Array.prototype.slice.call(forms).forEach(function (form) {
-      form.addEventListener(
-        "submit",
-        function (event) {
-          event.preventDefault();
-          event.stopPropagation();
-          if (!form.checkValidity()) {
-            // event.preventDefault();
-            // event.stopPropagation();
-            modal.style.display = "none";
-          }
-  
-          form.classList.add("was-validated");
-        },
-        // false
-      );
-    });
+import {
+  shoppingCartItems
+} from "./landingpage";
 
 
+window.onload = function () {
+  displayItemAmount();
+  loadCartinCheckout();
+  formFunction();
+}
 
-    document.getElementById("arrowBack").addEventListener("click", () => {
-      window.history.back();
-    })
+let sum: number = 0;
+let container: HTMLDivElement = document.querySelector(".bigWrapper");
+let cart: HTMLDivElement = document.createElement("div");
+cart.classList.add("checkout-cart");
+let totalContainer: HTMLDivElement = document.createElement("div");
+totalContainer.classList.add("total-container");
 
-    // Get the modal
-var modal = document.getElementById("myModal");
+let total: HTMLParagraphElement = document.createElement("p");
+total.classList.add("cart-total");
+container.prepend(totalContainer);
+totalContainer.appendChild(total);
 
-// Get the button that opens the modal
-var btn = document.querySelector("button");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-btn.addEventListener("click", () => {
-  console.log("btn");
-  modal.style.display = "block";
-});
-
-// When the user clicks on <span> (x), close the modal
-span.addEventListener("click", () => {
-  modal.style.display = "none";
-});
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+function loadCartinCheckout() {
+  for (let i = 0; i < shoppingCartItems.length; i++) {
+    let cartItem: HTMLDivElement = document.createElement("div");
+    cartItem.classList.add("checkout-cart-item");
+    let imgContainer: HTMLDivElement = document.createElement("div");
+    imgContainer.classList.add("checkout-img-container");
+    let img: HTMLImageElement = document.createElement("img");
+    img.setAttribute("src", shoppingCartItems[i].img);
+    cartItem.appendChild(imgContainer);
+    imgContainer.appendChild(img);
+    let productInfo: HTMLDivElement = document.createElement("div");
+    productInfo.classList.add("checkout-cart-info");
+    let productName: HTMLSpanElement = document.createElement("span");
+    productName.classList.add("checkout-cart-name")
+    productName.innerHTML = shoppingCartItems[i].name;
+    let productPrice: HTMLSpanElement = document.createElement("span");
+    productPrice.classList.add("checkout-cart-price")
+    productPrice.innerHTML = shoppingCartItems[i].price.toLocaleString() + " kr";
+    let productDetails: HTMLSpanElement = document.createElement("span");
+    productDetails.classList.add("checkout-cart-details")
+    productDetails.innerHTML = shoppingCartItems[i].details;
+    cartItem.appendChild(productInfo);
+    productInfo.appendChild(productName);
+    productInfo.appendChild(productPrice);
+    productInfo.appendChild(productDetails);
+    container.prepend(cart);
+    cart.appendChild(cartItem);
   }
 }
 
+getProductPrice();
 
-console.log("hejhej");
+function getProductPrice() {
+  shoppingCartItems.forEach(element => {
+    getTotal(element.price);
+  })
+}
+
+function getTotal(productprice: number) {
+  let price: number = productprice;
+  sum += price;
+  displayTotalAmount(sum);
+}
+
+function displayTotalAmount(sum: number) {
+  total.innerHTML = "Totalt: " + sum.toLocaleString() + " kr";
+}
+
+function formFunction() {
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll(".needs-validation");
+
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms).forEach(function (form) {
+    form.addEventListener(
+      "submit",
+      function (event) {
+        event.preventDefault();
+        event.stopPropagation();
+        if (!form.checkValidity()) {
+          modal.style.display = "none";
+        }
+
+        form.classList.add("was-validated");
+      },
+
+    );
+  });
 
 
 
-  
-//     // Fetch all the forms we want to apply custom Bootstrap validation styles to
-//     var forms = document.querySelectorAll(".needs-validation");
-  
-//     // Loop over them and prevent submission
-//     Array.prototype.slice.call(forms).forEach(function (form) {
-//       form.addEventListener(
-//         "submit",
-//         function (event) {
-//           if (!form.checkValidity()) {
-//             event.preventDefault();
-//             event.stopPropagation();
-//           }
-  
-//           form.classList.add("was-validated");
-//         },
-//         false
-//       );
-//     });
+  document.getElementById("arrowBack").addEventListener("click", () => {
+    window.history.back();
+  })
 
-//     document.getElementById("arrowBack").addEventListener("click", () => {
-//       window.history.back();
-//     })
+  // Get the modal
+  var modal = document.getElementById("myModal");
 
-//     // Get the modal
-// var modal = document.getElementById("myModal");
+  // Get the button that opens the modal
+  var btn = document.querySelector("button");
 
-// // Get the button that opens the modal
-// var btn = document.querySelector("button");
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
 
-// // Get the <span> element that closes the modal
-// var span = document.getElementsByClassName("close")[0];
+  // When the user clicks on the button, open the modal
+  btn.addEventListener("click", () => {
+    modal.style.display = "block";
+  });
 
-// // When the user clicks on the button, open the modal
-// btn.addEventListener("click", () => {
-//   console.log("btn");
-//   modal.style.display = "block";
-// });
+  // When the user clicks on <span> (x), close the modal
+  span.addEventListener("click", () => {
+    modal.style.display = "none";
+  });
 
-// // When the user clicks on <span> (x), close the modal
-// span.addEventListener("click", () => {
-//   modal.style.display = "none";
-// });
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
 
-// // When the user clicks anywhere outside of the modal, close it
-// window.onclick = function(event) {
-//   if (event.target == modal) {
-//     modal.style.display = "none";
-//   }
-// }
 
-// console.log("hejhej");
+}
 
 export default (function () {
   "use strict";
 });
-
-
-
